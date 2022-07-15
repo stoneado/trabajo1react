bc39459646feeddce59ee832aabb84926d14da10
stoneado <gabrielhuilipan12@gmail.com>
2022-07-14 21:01:18 -0300

    arranco stone

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-  
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<img src='https://c.tenor.com/DcjnNZ318NAAAAAM/rollingstones-music.gif'></img>
+
     </div>
   );
 }
